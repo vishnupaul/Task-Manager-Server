e374c17ca26fa6723d5e256c1a8859c279f2dd50
Vishnu Paul <paulvishnu567@gmail.com>
2021-07-21 07:46:07 +0530

    initial commit

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ app.use(express.json());
 
 app.use('/', require('./routes'));
 
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT}`);
 });
